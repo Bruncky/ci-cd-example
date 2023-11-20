0552ccacfba85c3a17a12d1e6616ca221a42bb79
Jose Salas <jmsalash@gmail.com>
2023-11-20 22:05:11 +0900

    Small change to API to check CD

--- api/fast.py
+++ api/fast.py
@@ -16,7 +16,7 @@ app.add_middleware(
 @app.get('/')
 def root():
     response = {
-        'greeting': 'Servus, griaß di!',    # This is a typical Bavarian greeting ;)
+        'greeting': 'Servus, griaß di, hola!',    # This is a typical Bavarian greeting ;)
         'timestamp': datetime.now()
     }
 
