3cd634b17dfe81b98ced7895b39d8ff4b122c5b2
Amanda Maurell <amanda.maurell@gmail.com>
2023-12-05 09:47:37 +0100

    changed face

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from api.fast import root
 
 st.write(root())
 
-st.write('This is obviously very simple, in reality one would rather use this page to display API results more nicely! :)')
+st.write('This is obviously very simple, in reality one would rather use this page to display API results more nicely! :D')
