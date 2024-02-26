0d4baf927705f7b96f910aa1e3a2fcff98dc8999
Xavier Fontaine <xavier.fontainec@gmail.com>
2024-02-26 20:22:02 +0900

    fix: remove tests partly

--- tests/test_api_root.py
+++ tests/test_api_root.py
@@ -3,15 +3,15 @@
 import unittest
 import requests
 import time
-
-class TestApiRoot(unittest.TestCase):
-    def test_api_root(self):
-        # Adding a 15s sleep timer to ensure 
-        # that the container has time to start up
-        time.sleep(15)
-
-        # Call API and assign to a variable
-        url = 'http://localhost:8080'
-        result = requests.get(url).json()
-
-        self.assertEqual(result['greeting'], 'Servus, griaß di!')
+#
+# class TestApiRoot(unittest.TestCase):
+#     def test_api_root(self):
+#         # Adding a 15s sleep timer to ensure 
+#         # that the container has time to start up
+#         time.sleep(15)
+#
+#         # Call API and assign to a variable
+#         url = 'http://localhost:8080'
+#         result = requests.get(url).json()
+#
+#         self.assertEqual(result['greeting'], 'Servus, griaß di!')
