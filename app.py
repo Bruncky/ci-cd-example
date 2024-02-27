eb3fe0bac30442768ae7df7811b93082abe053e0
Sandun Kanangama <sandunkanangama@gmail.com>
2024-02-27 09:50:49 +0900

    Added comment

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ from api.fast import root
 
 st.write(root())
 
+# Added some comments
 st.write('This is obviously very simple, in reality one would rather use this page to display API results more nicely! :)')
