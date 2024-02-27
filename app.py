948642ef56b3a57d8c868e7a64fc3782ed4d1778
Sandun Kanangama <sandunkanangama@gmail.com>
2024-02-27 09:51:44 +0900

    df

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from api.fast import root
 
 st.write(root())
 
-# Added some comments
+# Added some comments and added more
 st.write('This is obviously very simple, in reality one would rather use this page to display API results more nicely! :)')
