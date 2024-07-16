976992a832043d6c7a7de5029f4f3ff0f2c051f3
Nizar Malkiya <nizarrazin@gmail.com>
2024-07-16 09:48:37 +0200

    changed language

--- api/fast.py
+++ api/fast.py
@@ -16,7 +16,7 @@ app.add_middleware(
 @app.get('/')
 def root():
     response = {
-        'greeting': 'Servus, griaß di!',    # This is a typical Bavarian greeting ;)
+        'greeting': 'Bonjour!',    # This is a typical French greeting ;)
     }
 
     return response
