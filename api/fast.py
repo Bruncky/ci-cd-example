e072dbdf0809ba6a047039e741fb8f085774442f
Giordano <giomikecs17@gmail.com>
2024-08-19 11:18:57 -0500

    update api

--- api/fast.py
+++ api/fast.py
@@ -17,7 +17,8 @@ app.add_middleware(
 def root():
     response = {
         'greeting': 'Servus, griaß di!',    # This is a typical Bavarian greeting ;)
-        'timestamp': datetime.now()
+        'timestamp': datetime.now(),
+        'msg':'Hola Mundo'
     }
 
     return response
