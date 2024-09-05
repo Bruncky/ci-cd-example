acd4cbbcfc6230b372be9a737a9702cbf7770ed6
Malika67 <malika.elbaz67@gmail.com>
2024-09-05 19:16:16 +0200

    graph

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import streamlit as st
-
+import numpy as np
+import matplotlib.pyplot as plt
 from api.fast import root
 
 st.write(root())
 
 st.write('This is obviously very simple, in reality one would rather use this page to display API results more nicely! :)')
 st.write('Add anythin here')
+
+x = np.linspace(1, 20)
+y = np.sin(x)
+plt.plot(x,y)
