9dc63df313e4d3a28e85072c6c71324b2c2c7918
Malika67 <malika.elbaz67@gmail.com>
2024-09-05 19:18:42 +0200

    delete graph

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
+
 from api.fast import root
 
 st.write(root())
 
 st.write('This is obviously very simple, in reality one would rather use this page to display API results more nicely! :)')
-st.write('Add anythin here')
-
-x = np.linspace(1, 20)
-y = np.sin(x)
-fig = plt.plot(x,y)
-
-st.write(f'My plot {fig}')
+st.write('Add anything here and here')
