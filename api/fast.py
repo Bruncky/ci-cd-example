57e1d6effc385d9a00ef368b3fdf41069b960ee0
Malika67 <malika.elbaz67@gmail.com>
2024-09-09 19:39:36 +0200

    modf

--- api/fast.py
+++ api/fast.py
@@ -17,7 +17,9 @@ app.add_middleware(
 def root():
     response = {
         'greeting': 'Servus, griaß di!',    # This is a typical Bavarian greeting ;)
-        'timestamp': datetime.now()
+        #'timestamp': datetime.now()
+        
+        
     }
 
     return response
